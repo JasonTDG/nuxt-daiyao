20565fe3b4ecbd735228764369b977901aa8bdb2
Janyu <jason1230@taipeiads.com>
2023-05-02 14:22:09 +0800

    deploy setting

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    // app: {
-    //   baseURL: '/nuxt-github-pages/' // baseURL: '/<repository>/'
-    // },
+    app: {
+      baseURL: '/nuxt-daiyao/' // baseURL: '/<repository>/'
+    },
     // modules: [
     //     '@nuxtjs/style-resources',
     // ],
