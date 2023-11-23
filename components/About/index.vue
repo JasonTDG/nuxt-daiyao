aac4b647d1f4cf6907faf993781cb6465e677f77
Janyu <jason1230@taipeiads.com>
2023-11-23 15:54:44 +0800

    add favicon & remove remarks & fixed innerBanner

--- components/About/index.vue
+++ components/About/index.vue
@@ -5,11 +5,6 @@
 
 <script setup>
     import content from './content.vue'
-    // import innerBanner from '../InnerBanner/index.vue'
-    // const innerBannerUrl = ref("/nuxt-daiyao/image/about_banner.jpg")
-    // const innerBannerAlt = ref("關於我們")
-    // const innerBannerWidth = ref("1920")
-    // const innerBannerHeight = ref("460")
 </script>
 
 <style lang="scss" scoped>
